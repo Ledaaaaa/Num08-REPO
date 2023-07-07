d26c6416adac9642690c1e6215b1eef103e72f91
Ledaaaaa <124676430+Ledaaaaa@users.noreply.github.com>
2023-07-07 11:54:26 +0200

    altre modifiche

---
+++ progettoEsameFregoni/Assets/LogicScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+ public int playerScore;
+ public Text scoreText;
+
+
+[ContextMenu("Incrase Score")]
+ public void addScore(int scoreToAdd)
+ {
+    playerScore = playerScore + scoreToAdd;
+    scoreText.text = playerScore.ToString();
+ }
+
+}
