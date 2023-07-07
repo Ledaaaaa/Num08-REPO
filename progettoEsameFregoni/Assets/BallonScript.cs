b5891948c4de49fcd22e282c1ed48212ccf4960f
Ledaaaaa <124676430+Ledaaaaa@users.noreply.github.com>
2023-07-07 13:28:45 +0200

    screen game over

--- progettoEsameFregoni/Assets/BallonScript.cs
+++ progettoEsameFregoni/Assets/BallonScript.cs
@@ -6,19 +6,30 @@ public class BallonScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flatStrength;
+    public LogicScript logic;
+    public bool ballonIsAlive = true;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)== true)
+        if(Input.GetKeyDown(KeyCode.Space) == true && ballonIsAlive == true )
         {
             myRigidbody.velocity = Vector2.up * flatStrength;
         }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        ballonIsAlive =false;
+        animator.SetBool("ballonAlive", ballonIsAlive);
         
     }
 }
